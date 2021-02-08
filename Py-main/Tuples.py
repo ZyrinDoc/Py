b118a457904624dcb7614f1c57ad870acd22ac89
Goose.py <74518216+ZyrinDoc@users.noreply.github.com>
2021-02-08 10:50:01 -0500

    Add files via upload
    Added all files back after deleting them

---
+++ Py-main/Tuples.py
@@ -0,0 +1,9 @@
+coordinates = (4, 5)
+print(coordinates[1])
+
+
+
+
+
+
+
---
+++ Py-main/Var.py
@@ -0,0 +1,4 @@
+name = "George"
+age = "12"
+print("There once was a kid named", name)
+print(name, "is", age, "years old")
