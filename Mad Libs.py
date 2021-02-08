bd46b1375c5731c337a12c7e9a763c133f1fe8b4
Goose.py <74518216+ZyrinDoc@users.noreply.github.com>
2021-02-08 10:47:33 -0500

    Delete Mad Libs.py

--- Mad Libs.py
+++
@@ -1,8 +0,0 @@
-color = input("Name a colour: ")
-plural_noun = input("Name a Plural noun: ")
-celebrity = input("Name a Celebrity: ")
-
-
-print("Roses are", color)
-print(plural_noun, "are blue")
-print("I love", celebrity)
